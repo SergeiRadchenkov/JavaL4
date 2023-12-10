ee0b0649ac81c28b9167ad82678cdb1cf46ed466
Sergei <radchenkovsa@yandex.ru>
2023-12-10 17:32:48 +0300

    Лекция_4

---
+++ case01.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class case01 {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<Integer>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        System.out.println(ll);
+    }
+}
